74f4a25b4d319284035f2122adf91221a0679edc
tedtkang <tedtkang@gmail.com>
2021-02-18 10:15:50 +0000

    changing gitignore, routes and taking out hello page tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'hello_page/hello'
   root 'static_pages#home'
-  # get 'hello_page/hello'
   # root 'hello_page#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/hello_page_controller_spec.rb
+++ spec/controllers/hello_page_controller_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe HelloPageController, type: :controller do
   # HelloPageController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #hello" do
-    it "returns a success response" do
-      get :hello, params: {}, session: valid_session
-      expect(response).to be_success
-      expect(response.body).to include("Congratulations")
-      expect(response.content_type).to eq("text/html")
-    end
-  end
+#  describe "GET #hello" do
+#    it "returns a success response" do
+#      get :hello, params: {}, session: valid_session
+#      expect(response).to be_success
+#      expect(response.body).to include("Congratulations")
+#      expect(response.content_type).to eq("text/html")
+#    end
+#  end
 
-  describe "POST #hello" do
-    it "returns a success response" do
-      post :hello, params: {}, session: valid_session
-      expect(response).to be_success
-      expect(response.body).to include("Congratulations")
-      expect(response.content_type).to eq("text/html")
-    end
-  end
+#  describe "POST #hello" do
+#    it "returns a success response" do
+#      post :hello, params: {}, session: valid_session
+#      expect(response).to be_success
+#      expect(response.body).to include("Congratulations")
+#      expect(response.content_type).to eq("text/html")
+#    end
+#  end
 
 end
